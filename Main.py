637afa83eb977a8d19a693d5ce3c6adc94502b6c
Nickloay Gusarov <Kolya-gus2002@ya.ru>
2019-02-23 20:10:57 +0300

    added data

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import torch
 import torchvision
 import os
 from PIL import Image
+import PIL
 import torch.vision.transforms as transforms
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
@@ -16,60 +17,75 @@ num_classes = 10
 batch_size = 16
 learning_rate = 0.001
 
+# Установим размер классифицируемых изображений
 PIC_SIZE = 50
-DATA_PATH = ''
-MODEL_STORE_PATH = 'C:\\Users\Andy\PycharmProjects\pytorch_models\\'
+# Путь к предобработанным данным
+data_path = 'data//preprocessed//'
+# Путь, куда сохраним модель
+model_save_path = 'signs_classifier.pth'
 
-class PersonDataset(Dataset):
+
+class SignsDataset(Dataset):
+    """Road signs dataset."""
+
     def __init__(self, csv_file, root_dir, transform=None):
-        self.persons_frame = pd.read_csv(csv_file)
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.signs_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
 
-        self.persons = self.persons_frame['person'].unique()
-        self.person_indexes = {}
-        for i, person in enumerate(self.persons):
-            self.person_indexes[person] = i
+        # Cоздаём массив label->index и массив index->label
+        self.labels = self.signs_frame['label'].unique()
+        self.label_indexes = {}
+        for i, label in enumerate(self.labels):
+            self.label_indexes[label] = i
+
     def __len__(self):
-        return len(self.persons_frame)
+        return len(self.signs_frame)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.persons_frame.iloc[idx, 0])
+        # Загрузим изображение и приведём к размеру 50х50
+        img_name = self.root_dir + self.signs_frame.iloc[idx, 0]
         image = Image.open(img_name)
         image = image.resize((PIC_SIZE, PIC_SIZE), Image.ANTIALIAS)
-        image = np.array(image)
 
-        person_string = self.persons_frame.iloc[idx, 1]
-        person = self.person_indexes[person_string]
+        # В роли ответа будем давать номер label
+        label_string = self.signs_frame.iloc[idx, 1]
+        label = self.label_indexes[label_string]
 
+        # Применим преобразования изображения (например аугментацию)
         if self.transform:
             image = self.transform(image)
 
-        sample = {'image': image, 'person': person}
-
+        sample = {'image': image, 'label': label}
         return sample
 
-dataset = PersonDataset(DATA_PATH + 'persons.csv', DATA_PATH, torchvision.transforms.ToTensor())
+dataset = SignsDataset(data_path + 'labels.csv',
+                       data_path,
+                       torchvision.transforms.ToTensor())
 
 indicies = np.arange(len(dataset))
 
+
+# Разбиение датасета на train и validation
 train_sampler = SubsetRandomSampler(indicies[:int(len(dataset)*0.5)])
 validation_sampler = SubsetRandomSampler(indicies[int(len(dataset)*0.5):])
 
-personsTrainLoader = DataLoader(dataset, batch_size=16, sampler=train_sampler)
-personsValidationLoader = DataLoader(dataset, batch_size=32, sampler=validation_sampler)
+# DataLoader достаёт данные из dataset батчами
+signsTrainLoader = DataLoader(dataset, batch_size=16, sampler=train_sampler)
+signsValidationLoader = DataLoader(dataset, batch_size=32, sampler=validation_sampler)
 
-ToPIL = transforms.ToPILImage()
-
-batch = next(iter(personsTrainLoader))
-
-img = batch['image'][0]
-label_index = batch['person'][0]
-
-print(dataset.labels[label_index])
-plt.inshow(ToPIL(img))
 
 df = dataset.signs_frame
+classes_number = df['label'].nunique()
+print('Classes number:', classes_number)
+df.groupby('label')['file_name'].nunique()
 
 #СОЗДАЕМ СЕТЬ
 #
@@ -106,7 +122,7 @@ class ConvNet(nn.Module):
         return x
 
 cn = ConvNet(num_classes)
-batch = next(iter(personsTrainLoader))
+batch = next(iter(signsTrainLoader))
 cn(batch['image'])[0]
 
 
@@ -124,7 +140,7 @@ history = {'loss':[], 'val_loss':[]}
 loss_fn = torch.nn.CrossEntropyLoss()
 
 learning_rate = 1e-4
-optimizer = torch.optim.Adam(cn.parameters(), Ir = learning_rate)
+optimizer = torch.optim.Adam(cn.parameters(), lr=learning_rate)
 
 #цикл обучения
 i=0
@@ -132,7 +148,7 @@ i=0
 for epoch in tqdm_notebook(range(100)):
 
     running_loss = 0.0
-    for batch in personsTrainLoader:
+    for batch in signsTrainLoader:
         #current batch
 
         X_batch, y_batch = batch['image'], batch['label']
@@ -145,11 +161,11 @@ for epoch in tqdm_notebook(range(100)):
         loss.backward()
         optimizer.step()
 
-        ###########
+
         running_loss += loss.item()
         #пишем в лог каждые 50 батчей
         if i%50 == 49:
-            batch = next(iter(personsValidationLoader))
+            batch = next(iter(signsValidationLoader))
             X_batch, y_batch = batch['image'], batch['label']
             y_pred = cn(X_batch)
 
@@ -170,3 +186,14 @@ for epoch in tqdm_notebook(range(100)):
 
 cn = ConvNet(num_classes)
 cn.load_state_dict(torch.load(model_save_path))
+
+batch = next(iter(signsValidationLoader))
+predictions = cn(batch['image'])
+y_test = batch['label']
+
+
+#print(predictions, y_test)
+_, predictions = torch.max(predictions, 1)
+plt.imshow(PIL.ToPIL(batch['image'][0]))
+print('Gound-true:', dataset.labels[batch['label'][0]])
+print('Prediction:', dataset.labels[predictions[0]])
